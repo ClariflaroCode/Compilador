b5442baa4daacbbd165a211478608df79d235fa7
DCMateo <delcampomateo01@gmail.com>
2026-09-12 06:03:12 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String path ="C:\\Users\\Mateo\\eclipse-workspace\\pruebas\\bin\\pruebas\\codigo.txt";
+        String path ="C:\\Users\\Mateo\\eclipse-workspace\\pruebas\\bin\\codigo.txt";
 		Lexer lexLuthor = new Lexer(path);
 		int token=0;
 		while (token!=-1) { //ESTO HABRIA QUE PASARLO AL PARSER CREO
